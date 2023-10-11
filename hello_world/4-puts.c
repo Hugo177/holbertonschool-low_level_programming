b94133a4d2b6d167cd06d0dcab61088ff3e84bef
hugo177 <7472@holbertonstudents.com>
2023-10-11 03:49:22 -0700

    bhdzpooopja

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+
+char array[] = "\"Programming is like building a multilingual puzzle";
+
+puts(array);
+
+return (0);
+}

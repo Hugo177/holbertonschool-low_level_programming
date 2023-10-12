9ad83a8f9432cae76b3726c69b4ef5fa5fc8d215
hugo177 <7472@holbertonstudents.com>
2023-10-12 00:55:21 -0700

    jzhjnb

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+nclude<stdio.h>
+/**
+ *  * main - Entry point
+ *   * Return: Always 0 (Success)
+ *    */
+int main(void)
+{
+	char hi[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", hi);
+	return (0);
+}

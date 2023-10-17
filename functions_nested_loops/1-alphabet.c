d204fa3a93e68be70db248c6ac5fb44e0f3b4d69
Hugo177 <7472@holbertonstudents.com>
2023-10-17 05:43:36 -0700

    uhb

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include"main.h"
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+/**
+ *  print_alphabet - prints alphabet
+ *
+ *   Return: always 0;
+ *
+*/
+
+void print_alphabet(void)
+
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+
+	}
+
+	putchar('\n');
+
+}

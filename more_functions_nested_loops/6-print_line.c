698e7bbd6f778c9081bc9e84546e6f8a1847a6b3
Hugo177 <7472@holbertonstudents.com>
2023-10-20 02:23:07 -0700

    jnnij

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - draws a straight line;
+ * @n : number;
+ * Return: Alway 0.
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+
+	else
+	{
+		int j;
+
+		for (j = 0; j < n; j++)
+		{
+			putchar('_');
+																	}
+			putchar('\n');
+
+	}
+}

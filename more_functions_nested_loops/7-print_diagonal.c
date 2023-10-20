9bb8b1029210c9f0c14e8044f8b3c5602cc6f17a
Hugo177 <7472@holbertonstudents.com>
2023-10-20 02:29:17 -0700

    jnn

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagonal - Write a function that checks for uppercase character.
+ * @n : number
+ */
+void print_diagonal(int n)
+
+{
+	int i;
+
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+
+	for (i = 1; i <= n; i++)
+	{
+		int j;
+
+		for (j = 1; j < i; j++)
+		{
+																		putchar(' ');
+																	}
+
+		putchar('\\');
+		putchar('\n');
+																}
+}

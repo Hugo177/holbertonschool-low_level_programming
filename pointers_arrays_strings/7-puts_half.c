2041891d60fb0d0a245d182820de42dda82650f7
Hugo177 <7472@holbertonstudents.com>
2023-10-25 03:14:48 -0700

    jhbncaaaaa

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * puts_half - function that prints half of string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	char *p = str;
+	int i;
+	int mid;
+
+	while (*p != '\0')
+
+{
+		len++;
+		p++;
+}
+
+		mid = len / 2;
+
+		if (len % 2 != 0)
+		{
+			mid++;
+		}
+	for (i = mid; i < len; i++)
+	{
+		putchar(*(str + i));
+	}
+
+	putchar('\n');
+}
+

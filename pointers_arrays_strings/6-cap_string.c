8c6d9a42877aaea15167a256944d3ef4978a9883
Hugo177 <7472@holbertonstudents.com>
2023-10-27 11:27:05 -0700

    uhjufezfzezh

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -22,7 +22,7 @@ char *cap_string(char *s)
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
 					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
-			}	
+			}
 		}
 		count++;
 	}
